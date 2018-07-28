9f32edfdbc09ad6383742a2b9fa81e67c8dabec5
kashyap <ckkashyap@gmail.com>
2018-07-28 15:09:14 +0000

    micropython almost works

--- s2c.py
+++ s2c.py
@@ -2,16 +2,8 @@
 from compiler import clconvert
 from compiler import codegen
 from compiler import cpsconvert
-import re
 from compiler import string2ast
 import sys
-
-def py2clj(s):
-    s1 = re.sub(r'\[', '(', s)
-    s2 = re.sub(r']', ')', s1)
-    s3 = re.sub(r'\'', '', s2)
-    s4 = re.sub(r',', '', s3)
-    return s4
 
 if len(sys.argv) > 1:
     sourceName = sys.argv[1]
