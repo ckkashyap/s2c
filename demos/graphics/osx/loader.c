61fb6a2d7324aaf7e78b80928cd9f098bd2c55d1
CK Kashyap <ckkashyap@gmail.com>
2018-08-11 13:50:23 -0700

    works on osx

--- demos/graphics/osx/loader.c
+++ demos/graphics/osx/loader.c
@@ -12,7 +12,7 @@ RENDERER_TYPE loadRenderer()
     struct stat s;
     lstat(LIBRARY, &s);
 
-    system("make -f Makefile.posix render.dll");
+    system("make render.dll");
 
     if (ts.tv_nsec != s.st_mtimespec.tv_nsec)
     {
