eb95cc65e626f99a9dd98f03c0228edc04f9651e
Kashyap <ckkashyap@gmail.com>
2018-08-11 17:20:45 +0000

    windows build updated

--- demos/graphics/build.bat
+++ demos/graphics/build.bat
@@ -2,4 +2,4 @@
 
 cl /D_OS_IS_WINDOWS_ video.c /I include lib\x64\SDL2.lib  /Fe:video.exe
 python ../../s2c.py -dll render.scm > render.c
-cl /D_OS_IS_WINDOWS_ /LD render.c 
+cl /D_OS_IS_WINDOWS_ /LD render.c /Fe:_render.dll
